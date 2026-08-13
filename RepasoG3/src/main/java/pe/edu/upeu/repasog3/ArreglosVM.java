3da4668aee116d756fbf9c0ab36e10c8c981518a
dmamanipar <dmamanipar@unsa.edu.pe>
2026-08-13 14:47:03 -0500

    Ejemplo Venctor

--- RepasoG3/src/main/java/pe/edu/upeu/repasog3/ArreglosVM.java
+++ RepasoG3/src/main/java/pe/edu/upeu/repasog3/ArreglosVM.java
@@ -1,7 +1,22 @@
 package pe.edu.upeu.repasog3;
 
 public class ArreglosVM {
+
+    public static void meses(){
+        String[] mes={"Enero", "Febrero", "Marzo", "Abril", "Mayo",
+        "Junio", "Julio", "Agosto", "Setiembre",
+                "Octubre", "Noviembre", "Diciembre"};
+
+        System.out.println("Tamaño:"+mes.length);
+        for (String m:mes){
+            System.out.println(m);
+        }
+
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        meses();
     }
 }
