264b2c3ee4ed7668d8f472de708431bac0417a02
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-16 09:28:42 -0500

    Primer Avance

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
@@ -4,13 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pe.edu.upeu.sysventas.config.AppContext;
 
 import java.io.IOException;
 
 public class SysVentas extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SysVentas.class.getResource("hello-view.fxml"));
+        AppContext appContext=AppContext.getInstance();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(SysVentas.class.getResource("/view/main_producto.fxml"));
+        fxmlLoader.setControllerFactory(appContext::getBean);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         stage.setTitle("Hello!");
