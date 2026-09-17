fcfa8da798b84cba4439d75d8261d8ccfeafb8ee
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-17 16:44:21 -0500

    Interfaz

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentas.java
@@ -2,7 +2,9 @@ package pe.edu.upeu.sysventas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import pe.edu.upeu.sysventas.config.AppContext;
 
@@ -11,11 +13,15 @@ import java.io.IOException;
 public class SysVentas extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        Screen screen=Screen.getPrimary();
+
         AppContext appContext=AppContext.getInstance();
+        Rectangle2D dimen=screen.getBounds();
 
         FXMLLoader fxmlLoader = new FXMLLoader(SysVentas.class.getResource("/view/main_producto.fxml"));
         fxmlLoader.setControllerFactory(appContext::getBean);
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), dimen.getWidth(), dimen.getHeight()-60);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
